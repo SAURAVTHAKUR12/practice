9f1536c14d16b8237c6a77f3c787b006c93aaca5
SAURAV THAKUR <sauravthakur12@gmail.com>
2016-07-31 07:09:39 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,4 +1,4 @@
-_I am Saurav Thakur_
+*I am Saurav Thakur*
 I am pre final year Electrical Engineering undergrad at IITK
 I am working on android development and have experience of working on Neural Networks.
 I have done sport programming.
